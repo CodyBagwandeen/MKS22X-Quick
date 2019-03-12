961d504f96f28f5fdfdaffdfa2a363f335ac923a
CodyBagwandeen <cbagwandeen@stuy.edu>
2019-03-12 11:45:29 -0400

    init partition

--- Quick.java
+++ Quick.java
@@ -1,6 +1,14 @@
+
+
 public class Quick{
 
   public static int quickSelect(int[] data, int k){
+    return 0;
+  }
 
+  public int[] partition(int[] data, int start, int end){
+    int pivot = (int)(Math.random() * 10000 ) % data.length;
+
+    return data;
   }
 }
